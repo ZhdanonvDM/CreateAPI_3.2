5c39baa2e043538e62ea81fa7fa104acc39a41d7
ZhdanovDM <gdanovdm@gmail.com>
2023-07-24 19:59:04 +0500

    Commit 5

--- scripts421.sql
+++ scripts421.sql
@@ -1,8 +1,10 @@
-CREATE TABLE students
-(id bigint,
- age integer  check (age>16) default 20,
- name varchar (30) UNIQUE not null
-);
+ALTER table student add constraint age check (age>16);
+ALTER table student alter column age set default 20;
+ALTER table student add UNIQUE (name);
+ALTER table student alter column name SET not null;
+
+ALTER table faculty add UNIQUE (name, color);
+
 
 CREATE TABLE faculties
 (id bigint,
