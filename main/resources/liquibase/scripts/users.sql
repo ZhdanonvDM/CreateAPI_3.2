d7af154917e7b99b5db517d04b5f1bc392034ff8
ZhdanovDM <gdanovdm@gmail.com>
2023-07-28 20:14:46 +0500

    Commit

--- main/resources/liquibase/scripts/users.sql
+++ main/resources/liquibase/scripts/users.sql
@@ -2,6 +2,15 @@
 
 -- changeset ZhdanovDM:1
 CREATE TABLE users (
-    id serial,
-    email text
+id serial,
+email text
 )
+-- changeset ZhdanovDM:2
+CREATE INDEX users_email ON users (email)
+
+
+-- changeset ZhdanovDM:3
+CREATE INDEX student_name ON student (name)
+
+-- changeset ZhdanovDM:4
+CREATE INDEX faculty_name_color ON faculty (name, color)
